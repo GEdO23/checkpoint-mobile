b2ab9bd43ca903fb03f17ecd9690aa017bbf7f76
GEdO23 <rm99632@fiap.com.br>
2024-03-18 10:18:32 -0300

    Finalizando mudança de overview para login

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -7,43 +7,43 @@ import Details from '../screens/details';
 import Login from '../screens/login';
 
 export type RootStackParamList = {
-  Overview: undefined;
-  Details: { name: string };
+    Login: undefined;
+    Details: { name: string };
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Overview">
-        <Stack.Screen name="Overview" component={Login} />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={({ navigation }) => ({
-            headerLeft: () => (
-              <View style={styles.backButton}>
-                <Feather name="chevron-left" size={16} color="#007AFF" />
-                <Text style={styles.backButtonText} onPress={navigation.goBack}>
-                  Back
-                </Text>
-              </View>
-            ),
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen
+                    name="Details"
+                    component={Details}
+                    options={({ navigation }) => ({
+                        headerLeft: () => (
+                            <View style={styles.backButton}>
+                                <Feather name="chevron-left" size={16} color="#007AFF" />
+                                <Text style={styles.backButtonText} onPress={navigation.goBack}>
+                                    Back
+                                </Text>
+                            </View>
+                        ),
+                    })}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  backButton: {
-    flexDirection: 'row',
-    paddingLeft: 20,
-  },
-  backButtonText: {
-    color: '#007AFF',
-    marginLeft: 4,
-  },
+    backButton: {
+        flexDirection: 'row',
+        paddingLeft: 20,
+    },
+    backButtonText: {
+        color: '#007AFF',
+        marginLeft: 4,
+    },
 });
