85527357eb4cf14b64e0e0498501624fb81765b4
GEdO23 <rm99632@fiap.com.br>
2024-03-18 10:23:12 -0300

    Escondendo header

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -16,7 +16,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 export default function RootStack() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
+            <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen
                     name="Details"
