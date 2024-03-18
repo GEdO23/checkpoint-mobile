0ac426ae7e36d7d64d9553dd007c472d579c6f10
GEdO23 <rm99632@fiap.com.br>
2024-03-18 10:52:46 -0300

    :(

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,49 +1,87 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { RootStackParamList } from '../navigation';
+import { Button } from 'react-native-paper';
 
 type OverviewScreenNavigationProps = StackNavigationProp<RootStackParamList, 'Login'>;
 
 export default function Login() {
-  const navigation = useNavigation<OverviewScreenNavigationProps>();
+    const navigation = useNavigation<OverviewScreenNavigationProps>();
 
-  return (
-    <View>
+    return (
+        <View style={styles.container}>
 
+            <Text style={styles.title}>Log In</Text>
 
-
-
-
-
-
-
-
-
-
-
-
-
-        <View>
             <View>
-            <Text>Hello World</Text>
-            <Text>This is the first page of your app.</Text>
+                <View style={{display: 'flex', gap: 16, marginBottom: 24}}>
+                    <View style={styles.inputContainer}>
+                        <TextInput placeholder='Your email' />
+
+                    </View>
+
+                    <View style={styles.inputContainer}>
+                        <TextInput placeholder='Password' />
+
+                    </View>
+                    
+                </View>
+                
+                <Button mode='contained' style={styles.button}>Log In</Button>
+                
+                <Text style={styles.info}>Forgot password?</Text>
+
             </View>
-            <TouchableOpacity
-            onPress={() => navigation.navigate('Details', { name: 'Dan' })}>
-            <Text>Show Details</Text>
-            </TouchableOpacity>
+            
+
+            <View>
+                <Text>or</Text>
+
+                <View>
+                    <Text>Google</Text>
+                </View>
+
+                <View>
+                    <Text>Facebook</Text>
+                </View>
+            </View>
+            
+            
+            <Text>Don't have an account? Sign Up</Text>
+
         </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
 
     container: {
-        flex: 1,
-
+        flex: 1, justifyContent: 'center',
+        backgroundColor: '#FFF',
+        paddingHorizontal: 19
+    },
+    title: {
+        fontSize: 30, fontWeight: '600', color: '#171725',
+        marginBottom: 40
+    },
+    button: {
+        fontSize: 15, fontWeight: '600', color: '#FFF',
+        backgroundColor: '#0062FF',
+        borderRadius: 8,
+        paddingVertical: 4
+    },
+    inputContainer: {
+        borderWidth: 1.6, borderColor: '#E0E2E9', borderRadius: 8,
+        paddingVertical: 8, paddingHorizontal: 20,
+        display: 'flex', flexDirection: 'row', gap: 16,
+        fontSize: 14, fontWeight: '500', color: '#969AB8'
+        
+    },
+    info: {
+        fontSize: 15, fontWeight: '600', color: '#0062FF',
+        marginTop: 16, textAlign: 'right'
     }
 
 });
